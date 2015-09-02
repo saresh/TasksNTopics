bfe529a43ff44883642e1d987fad0bf376a754b2
O. Reshetko <saresh26@gmail.com>
2015-09-02 17:39:38 +0300

    setting hibernate + spring properties

---
+++ src/main/java/com/saresh/tnt/entity/Role.java
@@ -0,0 +1,19 @@
+package com.saresh.tnt.entity;
+
+public class Role {
+	private int id;
+	private String role;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+}
---
+++ src/main/java/com/saresh/tnt/entity/User.java
@@ -0,0 +1,61 @@
+package com.saresh.tnt.entity;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class User {
+	private int id;
+    private String login;
+    private String password;
+    private boolean enabled;
+    private List<Topic> topicsCreated;
+    private List<Topic> topicsAssigned;
+    private Set<Role> userRoles = new HashSet<Role>(0);
+    
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public boolean isEnabled() {
+		return enabled;
+	}
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+	public List<Topic> getTopicsCreated() {
+		return topicsCreated;
+	}
+	public void setTopicsCreated(List<Topic> topicsCreated) {
+		this.topicsCreated = topicsCreated;
+	}
+	public List<Topic> getTopicsAssigned() {
+		return topicsAssigned;
+	}
+	public void setTopicsAssigned(List<Topic> topicsAssigned) {
+		this.topicsAssigned = topicsAssigned;
+	}
+	public Set<Role> getUserRoles() {
+		return userRoles;
+	}
+	public void setUserRoles(Set<Role> userRoles) {
+		this.userRoles = userRoles;
+	}
+    
+    
+	
+}
